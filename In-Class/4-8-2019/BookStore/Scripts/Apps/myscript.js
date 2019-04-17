e8db56b854a011d4e3b0b9aed430b46f57a285db
Brandon Ramirez <S01167199@acad.tri-c.edu>
2019-04-17 12:40:40 -0400

    Completed In-Class 4-8-2019

---
+++ In-Class/4-8-2019/BookStore/Scripts/Apps/myscript.js
@@ -0,0 +1,56 @@
+﻿$(function () {
+    /*
+    ../Books - shows list of Books
+    ../ Authors - shows list of Authors
+    
+    ../Books/Search - returns a JSON array of all books written by the given Author
+        -This is a GET method
+        -Input parameter is a string (Author name)
+        -This method returns a JSON
+    
+    ../Books/DeleteByAuthor - returns a JSON object showing count and status of books deleted 
+    - This is a POST method
+    - Input parameter is a string (Author name)
+    -This method returns a JSON
+    */
+
+    $("#search").on("click", function () {
+
+        var searchstring = $("#AuthorSearch").val();
+        var url = "/Books/Search";
+
+        $.get(url, { "s": searchstring }, function (data) {
+
+            console.log(data);
+
+            if (data.length == 0) {
+
+                $("#results").text("No results found for the search string")
+
+            }
+            else {
+
+                $("#results").text("");
+
+                $.each(data, function (i, item) {
+
+                    var para = $("<p></p>").text(item.Title + " " + item.Year);
+                    $("#results").append(para);
+
+                });
+            }
+        });
+    });
+
+    $("#delete").on("click", function () {
+
+        var searchstring = $("#AuthorSearch").val();
+        var url = "/Books/DeleteByAuthor";
+
+        $.post(url, { "s": searchstring }, function (data) {
+
+            console.log(data);
+
+        });
+    });
+});
